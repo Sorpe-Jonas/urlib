d62a74b427525dd3b60561e6862d88bbfc905e22
Sorpe-Jonas <jonas@brighist.shop>
2024-12-22 21:40:10 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,4 @@
+// Este script pode ser usado para funcionalidades adicionais no frontend, se necessário.
+// Por exemplo, você pode adicionar validações extras ou interações com o DOM aqui.
+
+console.log("script2.js carregado com sucesso.");
